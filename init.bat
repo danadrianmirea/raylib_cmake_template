b60353e3316f0653c1d873d274e96456da5e9565
Dan Adrian Mirea <damirea84@gmail.com>
2024-06-28 17:17:11 +0300

    first commit

---
+++ init.bat
@@ -0,0 +1,7 @@
+echo "# raylib_vscode_template" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:danadrianmirea/raylib_vscode_template.git
+git push -u origin main
