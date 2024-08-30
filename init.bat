77dda8f6db56146fcf3985f13f140af982002a58
Dan Adrian Mirea <damirea84@gmail.com>
2024-08-30 23:40:38 +0300

    remove init.bat

--- init.bat
+++
@@ -1,7 +0,0 @@
-echo "# raylib_vscode_template" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:danadrianmirea/raylib_vscode_template.git
-git push -u origin main
